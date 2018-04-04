9eef97889d9c704173cfb21f9f6139a62ffafe07
hhiroot <hasmin2@gmail.com>
2018-04-04 17:52:24 +0900

    upgrade for Laser Position, renamed from TCP client to Cranedata specified

---
+++ src/main/java/com/streamsets/stage/origin/CraneDataCharacterSetChooserValues.java
@@ -0,0 +1,9 @@
+package com.streamsets.stage.origin;
+
+import com.streamsets.pipeline.api.base.BaseEnumChooserValues;
+
+public class CraneDataCharacterSetChooserValues extends BaseEnumChooserValues<CraneDataCharacterSet> {
+    public CraneDataCharacterSetChooserValues() {
+        super(CraneDataCharacterSet.class);
+    }
+}
